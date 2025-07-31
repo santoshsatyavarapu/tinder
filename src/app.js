e9bebbe70d313a6807141c12bdf33114d5218a40
santoshsatyavarapu <santoshsatyavarapu123@gmail.com>
2025-07-31 21:20:18 +0530

    explored various http methoss

--- src/app.js
+++ src/app.js
@@ -2,9 +2,29 @@ const express = require("express");
 
 const app = express();
 
+
+
+
+app.get("/santosh", (req, res) => {
+  res.send({name:"santosh",age:25});
+});
+
+app.post("/santosh", (req, res) => {
+  res.send("Deatils Uploaded");
+});
+
+app.delete("/santosh", (req, res) => {
+  res.send("Deatils removed");
+});
+
 app.use((req, res) => {
   res.send("serveris running");
 });
+
+
+
+
+
 
 app.listen(3001, () => {
   console.log("severing is running");
